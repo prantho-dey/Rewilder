f46a0f91222994b064abcdef87599ca6b525348c
Prantho <pranthodey89@gmail.com>
2021-07-20 21:31:38 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+(function ($) {
+
+    'use strict';
+
+	/*------------------------------------
+    countdown
+	--------------------------------------*/
+	if (jQuery("[data-countdown]").length > 0) {
+		$('[data-countdown]').each(function () {
+			var $this = $(this), finalDate = $(this).data('countdown');
+			$this.countdown(finalDate, function (event) {
+				$this.html(event.strftime('<span class="cdown days"><p>Days</p> <span class="time-count">%-D</span></span> <span class="cdown hour"><p>Hours</p> <span class="time-count">%-H</span></span> <span class="cdown minutes"><p>Minutes</p> <span class="time-count">%M</span></span> '));
+			});
+		});
+	};
+
+	// ================ Accordion =================
+	$(".single-accordion").click(function(){
+		$(this).toggleClass("active").children('.accordion-body').slideToggle().parents('.single-accordion').siblings().removeClass('active').children('.accordion-body').slideUp();
+	});
+
+
+	// PreLoader 
+	// var PreLoader = document.getElementById('preloader');
+
+	// function myFuncatio() {
+	// 	PreLoader.style.display = 'none'
+	// };
+	
+	/*------------------------------------
+        scrollUp
+	--------------------------------------*/
+	$(window).scroll(function () {
+		if ($(this).scrollTop() !== 0) {
+			$('#scrollUp').fadeIn();
+		} else {
+			$('#scrollUp').fadeOut();
+		}
+    });
+    
+	$('#scrollUp').on('click', function () {
+		$("html, body").animate({scrollTop: 0}, 600);
+		return false;
+	});
+
+	
+	$(window).load(function() {
+		$(this).scrollTop(0);
+		$(".loader-wrapper").fadeOut(60);//The time it take
+		$('body').addClass('preloader')
+	});
+
+
+
+})(jQuery);
+
+// $(window).load(function() {
+//     $(this).scrollTop(0);
+//     $(".preloader").fadeOut(60);//The time it take
+//     $('body').addClass('preloader')
+// });
+
